7db7129c7a57cfbb8610db6d184592007e69454f
Yu-Gi-Oh-Platform OzG71LXXeKVjzhP8LLWaYM4bo <177896519+OzG71LXXeKVjzhP8LLWaYM4bo@users.noreply.github.com>
2024-09-01 17:18:00 +1000

    a

--- app.py
+++ app.py
@@ -9,20 +9,16 @@ def home():
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    # Get the IP address from the query parameters
     ip_address = request.args.get('ip')
 
-    # Basic validation for the IP address
     if not ip_address:
         return jsonify({"error": "No IP address provided"}), 400
     
     url = f"https://freeipapi.com/api/json/{ip_address}"
-
-
     response = requests.get(url)
     data = response.json()
 
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5002)
