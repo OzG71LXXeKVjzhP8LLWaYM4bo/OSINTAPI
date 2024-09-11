4324d5ba9b59a5677719ad471547cff927d42920
Yu-Gi-Oh-Platform OzG71LXXeKVjzhP8LLWaYM4bo <177896519+OzG71LXXeKVjzhP8LLWaYM4bo@users.noreply.github.com>
2024-09-11 19:29:12 +1000

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Welcome to the Flask API!"})
-
 @app.route('/api/ipdata', methods=['GET'])
+
 def get_data():
     ip_address = request.args.get('ip')
 
@@ -20,34 +17,31 @@ def get_data():
 
     return jsonify(data)
 
+@app.route("/api/startscan", methods = ["POST"])
+def start():
+    target = requests.args.get("")
+
+    if not target:
+        return jsonify({"error": "No target provided"}), 400
+    
+@app.route("/api/scansummary", methods = ["POST"])
+def summary():
+    target = requests.args.get("")
+
+    if not target:
+        return jsonify({"error": "No IP address provided"}), 400
+    
+@app.route("/api/scaneventresults", methods = ["POST"])
+def eventresults():
+    target = requests.args.get("")
+
+    if not target:
+        return jsonify({"error": "No IP address provided"}), 400
+    
+@app.route("/api/get", methods = ["GET"])
+def home():
+    return jsonify({"success": "api is working"})
+    
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
-
-@app.route("/api/startscan", methods = ["POST"])
-def start():
-    target = requests.args.get("")
-
-    if not target:
-        return jsonify({"error": "No IP address provided"}), 400
-    
-@app.route("/api/startscan", methods = ["POST"])
-def start():
-    target = requests.args.get("")
-
-    if not target:
-        return jsonify({"error": "No IP address provided"}), 400
-    
-@app.route("/api/startscan", methods = ["POST"])
-def start():
-    target = requests.args.get("")
-
-    if not target:
-        return jsonify({"error": "No IP address provided"}), 400
-    
-@app.route("/api/startscan", methods = ["POST"])
-def start():
-    target = requests.args.get("")
-
-    if not target:
-        return jsonify({"error": "No IP address provided"}), 400
-    
+    app.run(debug=True, host = "localhost", port=5002)
