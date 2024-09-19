ad1f54ad692aed41149b0aea9f550d581d2aeb7f
Yu-Gi-Oh-Platform OzG71LXXeKVjzhP8LLWaYM4bo <177896519+OzG71LXXeKVjzhP8LLWaYM4bo@users.noreply.github.com>
2024-09-19 14:01:42 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 import re
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/ipdata', methods=['POST'])
 def get_data():
